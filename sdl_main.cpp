f5d697e35edaef58cc3388cc987c6ae4309f28a5
j <195549@student.pwr.wroc.pl>
2013-11-12 18:23:50 +0100

    Add few lights to the SDL frontend to make it prettier.

--- sdl_main.cpp
+++ sdl_main.cpp
@@ -20,13 +20,21 @@ int main( int argc, char * argv[] )
     vec3_set( sphere.color, 1.0f, 1.0f, 1.0f );
     sphere.radius = 50.0f;
 
-    t_light light;
-    vec3_set( light.position, 10.0f, 75.0f, 1.0f );
-    vec3_set( light.color, 1.0f, 1.0f, 1.0f );
+    const float light_distance = 30.0f;
+
+    t_vec3 shift;
+    t_light lights[4];
+    vec3_set( lights[0].color, 1.0f, 0.25f, 0.25f );
+    vec3_set( lights[1].color, 0.25f, 0.25f, 1.0f );
+    vec3_set( lights[2].color, 0.25f, 1.0f, 0.25f );
+    vec3_set( lights[3].color, 0.25f, 0.25f, 0.25f );
+
+    int n_lights = sizeof( lights ) / sizeof( t_light );
 
     uint32_t img[ width ][ height ];
     memset( &img, 0, sizeof( img ) );
 
+    float t = 0.0f;
     for( ;; )
     {
         SDL_Event event;
@@ -41,13 +49,30 @@ int main( int argc, char * argv[] )
             }
         }
 
-        cuda_main( width, height, (uint32_t *)img, &sphere, 1, &light, 1 );
+        for( int i = 0; i < n_lights; ++i )
+            vec3_dup( lights[i].position, sphere.position );
+
+        vec3_set( shift, (sphere.radius + light_distance) * cosf(t), 0.0f, (sphere.radius + light_distance) * sinf(t) );
+        vec3_add( lights[0].position, shift );
+
+        vec3_set( shift, 0.0f, (sphere.radius + light_distance) * cosf(t), (sphere.radius + light_distance) * sinf(t) );
+        vec3_add( lights[1].position, shift );
+
+        vec3_set( shift, (sphere.radius + light_distance) * cosf(t * 0.75f), (sphere.radius + light_distance) * cosf(t * 0.75f), (sphere.radius + light_distance) * sinf(t * 0.75f) );
+        vec3_add( lights[2].position, shift );
+
+        vec3_set( shift, 0.0f, 0.0f, sphere.radius + 200.0f );
+        vec3_add( lights[3].position, shift );
+
+        cuda_main( width, height, (uint32_t *)img, &sphere, 1, lights, n_lights );
 
         SDL_UpdateTexture( texture, NULL, img, width * sizeof( uint32_t ) );
         SDL_RenderClear( renderer );
         SDL_RenderCopy( renderer, texture, NULL, NULL );
         SDL_RenderPresent( renderer );
         SDL_Delay( 16 );
+
+        t += 0.3;
     }
 
     SDL_DestroyWindow( window );
