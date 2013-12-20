4e8c0091efae0d9e7e7f419420c770cfd5b42687
j <195549@student.pwr.wroc.pl>
2013-12-20 01:30:04 +0100

    MSVC compatibility.

--- sdl_main.cpp
+++ sdl_main.cpp
@@ -153,7 +153,7 @@ int main( int argc, char * argv[] )
     vec3_set( lights[0].color, 1.0f, 1.0f, 1.0f );
     vec3_set( lights[0].position, width / 2.0f, height / 2.0f, 500.0f );
 
-    uint32_t img[ width ][ height ];
+    uint32_t * img = (uint32_t *)malloc( width * height * sizeof( uint32_t ) );
     memset( &img, 0, sizeof( img ) );
 
     float t = 0.0f;
